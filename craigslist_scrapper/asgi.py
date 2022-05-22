0814d981f959a0e3c5dde7217d92d779b8a62f05
Omar Bassam <omar.bassam88@gmail.com>
2022-05-22 23:12:47 +0200

    Renamed project correctly

--- craigslist_scrapper/asgi.py
+++ craigslist_scrapper/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for craiglist_scrapper project.
+ASGI config for craigslist_scrapper project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'craiglist_scrapper.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'craigslist_scrapper.settings')
 
 application = get_asgi_application()
--- craigslist_scrapper/urls.py
+++ craigslist_scrapper/urls.py
@@ -1,4 +1,4 @@
-"""craiglist_scrapper URL Configuration
+"""craigslist_scrapper URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
